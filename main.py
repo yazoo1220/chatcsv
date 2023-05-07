0d7fc148fa11f76052149b54f169121846fa71ff
yazoo1220 <podman1220@hotmail.com>
2023-05-08 00:01:41 +0300

    Update main.py removed[] from the result

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ if ask_button:
     chat_history = []
     prefix = f'You are the best explainer. please answer in {language}. User: '
     response = agent({"input":user_input})
-    result = json.dumps(response['intermediate_steps'], indent=2)
+    result = json.dumps(response['intermediate_steps'], indent=2)..replace('[', '').replace(']', '')
     
     st.session_state.past.append(user_input)
     st.session_state.generated.append(result)
